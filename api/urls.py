a3b829c0628fb2617f6b78f37ab90e3c8546f187
Sofia Pacheco <spacheco1@uc.cl>
2020-05-02 16:18:17 -0400

    Solved post url

--- api/urls.py
+++ api/urls.py
@@ -9,12 +9,13 @@ router.register(r'ingrediente', views.IngredientViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    #path('hamburguesa/', views.BurgerAPIView.as_view())
     path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('hamburguesa', views.BurgerViewSet.as_view({'post':'create'})),
     path('hamburguesa/<pk>/ingrediente/<id>', 
         views.BurgerViewSet.as_view({'put': 'add_ingredient', 'delete': 'delete_ingredient'})),
     path('hamburguesa/<pk>',
         views.BurgerViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'update'})),
+    path('ingrediente', views.IngredientViewSet.as_view({'post':'create'})),
     path('ingrediente/<pk>',
         views.IngredientViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}))
 ]
